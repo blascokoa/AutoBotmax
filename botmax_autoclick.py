e6ad708fd4797a576002d393f7415aada7a26d4f
Blasco <blascokoa1@gmail.com>
2019-01-25 21:01:09 +0100

    AutoBotBax 1.0
    added cancel button
    added anti-ban function

--- botmax_autoclick.py
+++ botmax_autoclick.py
@@ -29,9 +29,12 @@ def main():
     input("Put the cursor on the BUY ORDER SIZE and press ENTER")
     buy_size_x, buy_size_y = pyautogui.position()
     print(f"Coordinate recorded: {buy_size_x},{buy_size_y}")
-    input("Put the cursor on the BUY BUTTON and press ENTER")
-    buy_button_x, buy_button_y = pyautogui.position()
-    print(f"Coordinate recorded: {buy_button_x},{buy_button_y}")
+    input("Put the cursor on the BUY BUTTON ON TOP LEFT and press ENTER")
+    buy_button_x_top, buy_button_y_top = pyautogui.position()
+    print(f"Coordinate recorded: {buy_button_x_top},{buy_button_y_top}")
+    input("Put the cursor on the BUY BUTTON ON BOTTOM RIGHT and press ENTER")
+    buy_button_x_bottom, buy_button_y_bottom = pyautogui.position()
+    print(f"Coordinate recorded: {buy_button_x_bottom},{buy_button_y_bottom}")
 
     # SELL Options
     input("Put the cursor on the BOOK ORDER SELL and press ENTER")
@@ -40,29 +43,61 @@ def main():
     input("Put the cursor on the SELL ORDER SIZE and press ENTER")
     sell_size_x, sell_size_y = pyautogui.position()
     print(f"Coordinate recorded: {sell_size_x},{sell_size_y}")
-    input("Put the cursor on the SELL BUTTON and press ENTER")
-    sell_button_x, sell_button_y = pyautogui.position()
-    print(f"Coordinate recorded: {sell_button_x},{sell_button_y}")
+    input("Put the cursor on the SELL BUTTON ON TOP LEFT and press ENTER")
+    sell_button_x_top, sell_button_y_top = pyautogui.position()
+    print(f"Coordinate recorded: {sell_button_x_top},{sell_button_y_top}")
+    input("Put the cursor on the SELL BUTTON ON BOTTOM RIGHT and press ENTER")
+    sell_button_x_bottom, sell_button_y_bottom = pyautogui.position()
+    print(f"Coordinate recorded: {sell_button_x_bottom},{sell_button_y_bottom}")
+
+    # Cancel Button
+    input("Put the cursor on the CANCELL BUTTON ON BOTTOM RIGHT and press ENTER")
+    cancel_button_x_bottom, cancel_button_y_bottom = pyautogui.position()
+    print(f"Coordinate recorded: {cancel_button_x_bottom},{cancel_button_y_bottom}")
+    input("Put the cursor on the CANCELL BUTTON ON TOP LEFT and press ENTER")
+    cancel_button_x_top, cancel_button_y_top = pyautogui.position()
+    print(f"Coordinate recorded: {cancel_button_x_top},{cancel_button_y_top}")
     secs_between_clicks = random.uniform(0.1, 0.5)
     secs_between_clicks = 0
     clicks = 1
 
+    print("----- Bot Starts running ----- ")
+    print("----- Credits to @dblama ----- ")
+
     for i in range(loops):
+
+
         # Buy serie
 
         pyautogui.click(x=buy_price_x, y=buy_price_y, clicks=clicks, interval=secs_between_clicks, button='left')
         time.sleep(random.uniform(0.2, 0.5))
         pyautogui.click(x=buy_size_x, y=buy_size_y, clicks=clicks, interval=secs_between_clicks, button='left')
         time.sleep(random.uniform(0.2, 0.5))
-        pyautogui.click(x=buy_button_x, y=buy_button_y, clicks=clicks, interval=secs_between_clicks, button='left')
+        pyautogui.click(x=random.uniform(buy_button_x_bottom, buy_button_x_top), y=random.uniform(buy_button_y_top,
+                                    buy_button_y_bottom), clicks=clicks, interval=secs_between_clicks, button='left')
+
+        # Cancel
+        pyautogui.click(x=random.uniform(cancel_button_x_bottom, cancel_button_x_top), y=random.uniform(cancel_button_y_top,
+                                    cancel_button_y_bottom), clicks=clicks, interval=secs_between_clicks, button='left')
+
         # Sell serie
         time.sleep(random.uniform(0.2, 0.5))
         pyautogui.click(x=sell_price_x, y=sell_price_y, clicks=clicks, interval=secs_between_clicks, button='left')
         time.sleep(random.uniform(0.2, 0.5))
         pyautogui.click(x=sell_size_x, y=sell_size_y, clicks=clicks, interval=secs_between_clicks, button='left')
         time.sleep(random.uniform(0.2, 0.5))
-        pyautogui.click(x=sell_button_x, y=sell_button_y, clicks=clicks, interval=secs_between_clicks, button='left')
+        pyautogui.click(x=random.uniform(sell_button_x_top, sell_button_x_bottom), y=random.uniform(sell_button_y_top,
+                                    sell_button_y_bottom), clicks=clicks, interval=secs_between_clicks, button='left')
         time.sleep(random.uniform(0.2, 0.5))
+
+        # Cancel
+        pyautogui.click(x=random.uniform(cancel_button_x_bottom, cancel_button_x_top), y=random.uniform(cancel_button_y_top,
+                                    cancel_button_y_bottom), clicks=clicks, interval=secs_between_clicks, button='left')
+
+        print(f"Running the loop {i+1} of {loops}")
+
+    print("----- Bot finish ----- ")
+    print("----- Credits to @dblama ----- ")
 
 
 if __name__ == '__main__':
